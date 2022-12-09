771f9c188537c338007edbae2563d99e80c2865f
efforthye <efforthye@gmail.com>
2022-12-09 11:28:01 +0900

    커뮤니티 컴포넌트 생성 - 수정 해야함

--- Maple/mapleweb/src/App.js
+++ Maple/mapleweb/src/App.js
@@ -8,9 +8,12 @@ import BugReportContainer from "./components/Support/BugReport/Container";
 function App() {
   return (
     <div>
-      <HomeComponet />
-      <UserComponet />
+      {/* <HomeComponet /> */}
+      {/* <UserComponet /> */}
+
+      {/* 나중에 합칠 때 홈 안에 커뮤니티 컴포넌트를 넣어야 한다. */}
       <CommunityComponet />
+      
       <Link to={"/Support"}>고객지원</Link>
       <Routes>
         <Route path="/Support/*" element={<SupportComponet />}></Route>
@@ -20,6 +23,7 @@ function App() {
         ></Route>
       </Routes>
       <AdministratorComponet />
+
     </div>
   );
 }

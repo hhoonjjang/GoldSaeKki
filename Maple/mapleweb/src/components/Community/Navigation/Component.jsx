401344a33f6eeb055b90eb3f19839adc407a78a5
efforthye <efforthye@gmail.com>
2022-12-09 11:28:01 +0900

    커뮤니티 컴포넌트 생성 - 수정 해야함

---
+++ Maple/mapleweb/src/components/Community/Navigation/Component.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const NavigationComponent = () => {
+    return (
+        <NavigationBox>
+            나는 커뮤니디 안의 네비바예요
+        </NavigationBox>
+    );
+};
+
+export default NavigationComponent;
+
+const NavigationBox = styled.div`
+    font-size: 20px;
+`;

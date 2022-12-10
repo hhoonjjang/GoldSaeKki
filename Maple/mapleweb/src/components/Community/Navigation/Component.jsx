c874f3cf2f7c48d34976001b2041a4946b6c771f
efforthye <efforthye@gmail.com>
2022-12-10 23:29:20 +0900

    네비게이션 라우터 임시완성

--- Maple/mapleweb/src/components/Community/Navigation/Component.jsx
+++ Maple/mapleweb/src/components/Community/Navigation/Component.jsx
@@ -1,26 +1,122 @@
 import styled from 'styled-components';
+// import { Link, Routes, Route } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useRef, useState } from 'react';
 
-const NavigationComponent = ({ category }) => {
+// # 네비게이션 컴포넌트 사용을 위해 상위 컴포넌트에서 이처럼 카테고리와  
+// 라우터 주소를 작성하고, categorys라는 이름의 props로 보내주면 된다.
+// const CATEGORY = [
+//     { name: '자유게시판', label: "Free", link: "/Free" },
+//     { name: '정보게시판', label: "Information", link: "/Information" },
+//     { name: '토론게시판', label: "TopicDiscussion", link: "/TopicDiscussion" },
+//     { name: '메이플아트', label: "Art", link: "/Art" },
+//     { name: '메이플코디', label: "Coordination", link: "/Coordination" }
+// ];
+// # 어떤 컴포넌트를 불러올 지는 상위 컴포넌트의 Route를 통해 지정해준다.
+// 
 
-    console.log(category);
+const NavigationComponent = ({ categorys }) => {
+
+    const location = useLocation();
+
+    // 주소 이름은 locationName.current를 통해 가져올 수 있다.
+    const locationName = useRef("");
+    // 현재 몇 번째 카테고리인지 nowRouterIdx.current를 통해 가져올 수 있다.
+    const nowRouterIdx = useRef(0);
+
+    // 페이지 리렌더링
+    const [_, render] = useState(true);
+
+    // 라우터 주소가 바뀌면 해당 라우터 이름을 반환한다.
+    useEffect(() => {
+
+        render(!_);
+
+        const locationArr = location.pathname.split("/");
+        locationName.current = locationArr[locationArr.length - 1];
+
+        // 현재 라우터 이름이 카테고리의 몇번째인지 알아낸다.
+        // locationName.current
+        categorys.map((category, index) => {
+
+            if (category.label === locationName.current) {
+                nowRouterIdx.current = index;
+            }
+
+        });
+
+    }, [location])
+
 
     return (
-        <NavigationWrap>
-            {/* 네비바 영역 */}
-            <NavigationBox>
-                {category.map((item, idx) => {
-                    return (
-                        // item[0] : 라우터 주소, 버튼 클릭시 해당 라우터로 이동한다.(리액트 라우터)
-                        <LiBox>
-                            <li key={`category-${item[0][idx]}`}>
-                                {item[1]}
-                            </li>
+        <NavigationWrap className='mnb_wrap'>
+
+            <NavigationBox className='div_inner2'>
+
+                {categorys.map((item, idx) => {
+                    return (<>
+                        <LiBox key={`listbox-${item.label}`}>
+                            <Link
+                                key={`list-${item.label}`}
+                                // to : 해당 카테고리 라우터로 이동한다.
+                                to={`./${item.label}`}
+                                // className : 내가 선택한 카테고리(라우터에서 가져옴?)랑 같으면 띄우도록 해야한다.
+                                className={`${idx === nowRouterIdx.current ? "active" : ""}`}>
+                                <CategoryLi key={`category-${item.label}`}>{item.name}</CategoryLi>
+                            </Link>
+
                         </LiBox>
+                    </>
                     )
                 })}
+
+                <span className='mnb_line'></span>
+
             </NavigationBox>
+
+            <style jsx>{`
+                .mnb_list{
+                    width:100%; 
+                    float:left; 
+                    margin-top:1px;
+                }
+                .mnb_list li{
+                    float:left; 
+                    height:64px; 
+                    vertical-align: middle; 
+                    line-height: 64px; 
+                    margin-left:54px; 
+                    font-size:16px; 
+                    color:#888888;
+                    position:relative;
+                }
+                .mnb_list li:first-child{
+                    margin-left:0;
+                }
+                .mnb_list a{
+                    color:#666; 
+                    width:100%; 
+                    height:100%; 
+                    float:left;
+                }
+                .mnb_list li.active a{
+                    color:#333; 
+                    font-weight:bold;
+                }
+                .mnb_line{
+                    position:absolute; 
+                    left:32px; 
+                    bottom:-1px; 
+                    height:5px; 
+                    background-color:#434343; width:20px;
+                }
+            `}</style>
+
+            {/* {mnb_setting()} */}
         </NavigationWrap>
+
     );
+
 };
 
 export default NavigationComponent;
@@ -33,15 +129,17 @@ const NavigationWrap = styled.div`
     top: 62px;
     border-top: 1px solid #ebebeb;
     border-bottom: 1px solid #ebebeb;
+
+    
+    width:100%; 
+    float:left; 
 `;
 
 const NavigationBox = styled.div`
     margin: 0 auto;
     width: 1200px;
-    background-color: rgb(245,245,245);
     height: inherit;
-
-
+    position: relative;
 `;
 
 const LiBox = styled.div`
@@ -56,10 +154,26 @@ const LiBox = styled.div`
         line-height: 64px;
         text-align: left;
         background-color: white;
-        margin-right: 45px;
-        a{
-            color: #666;
-            text-decoration: none;
+        margin-right: 44px;
+    }
+    a{
+        color: #666;
+        text-decoration: none;
+
+        // a 호버
+        &:hover{
+            color: #242424;
         }
+        /* a중에서 active인 class를 가진 li */
+        &.active li{
+            color: black;
+            font-weight: 600;
+        }
+
     }
 `;
+
+const CategoryLi = styled.li`
+
+`;
+
--- Maple/mapleweb/src/components/Community/index.jsx
+++ Maple/mapleweb/src/components/Community/index.jsx
@@ -1,23 +1,50 @@
 import styled from "styled-components";
+// import { Link, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import NavigationComponent from "./Navigation/Component";
+import FreeImg from './images/자유.png';
+import InformationImg from './images/정보.png';
+import TopicDiscussionImg from './images/토론.png';
+import ArtImg from './images/아트.png';
+import CoordinationImg from './images/코디.png';
+
+const CATEGORY = [
+  { name: '자유게시판', label: "Free", link: "/Free" },
+  { name: '정보게시판', label: "Information", link: "/Information" },
+  { name: '토론게시판', label: "TopicDiscussion", link: "/TopicDiscussion" },
+  { name: '메이플아트', label: "Art", link: "/Art" },
+  { name: '메이플코디', label: "Coordination", link: "/Coordination" }
+];
 
 const CommunityComponet = () => {
 
   // 리듀서에 넣어서 리듀서에서 가져오기..?
-  const CATEGORY = [
-    ["Free", '자유게시판'],
-    ["Information", '정보게시판'],
-    ["TopicDiscussion", '토론게시판'],
-    ["Art", '메이플아트'],
-    ["Coordination", '메이플코디']
-  ];
 
   return (
     <CommunityBox>
 
       {/* 네비게이션 사용시 카데고리와 라우터 값을 배열안의 객체로 보내준다. */}
-      <NavigationComponent category={CATEGORY} />
+      <NavigationComponent categorys={CATEGORY} />
+
+      {/* 여기에 나머지 라우터를 띄움 */}
+      <Routes>
+        <Route path="/Free" element={
+            <img src={FreeImg} alt="임시 자유게시판" />
+        }></Route>
+        <Route path="/Information" element={
+            <img src={InformationImg} alt="임시 정보게시판" />
+        }></Route>
+        <Route path="/TopicDiscussion" element={
+            <img src={TopicDiscussionImg} alt="임시 토론게시판" />
+        }></Route>
+        <Route path="/Art" element={
+            <img src={ArtImg} alt="임시 토론게시판" />
+        }></Route>
+        <Route path="/Coordination" element={
+            <img src={CoordinationImg} alt="임시 토론게시판" />
+        }></Route>
+      </Routes>
 
     </CommunityBox>
   );
@@ -29,3 +56,4 @@ const CommunityBox = styled.div`
   min-height : 1600px;
   background-color: #e6e6e6;
 `;
+

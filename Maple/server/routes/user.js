e49d98ca2a09bc56429da7145f3d4ace33d96d1b
kawonlee <shadow4634@naver.com>
2022-12-12 11:38:13 +0900

    1212

---
+++ Maple/server/routes/user.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+import db from "../models/index.js";
+
+// router.post("/regist", (req, res) => {
+//     tempuserId = req.body.userId;
+//     tempuserPw = req.body.usepw;
+//     tempuserName = req.body.userName;
+// }).then(data)
+
+export default router;

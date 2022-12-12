da1867034514046e874e4eaf093db32368334430
Koh-U-Seok <kosh1668@naver.com>
2022-12-12 11:36:26 +0900

    1212

--- Maple/mapleweb/src/App.js
+++ Maple/mapleweb/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 
-
 import HeaderComponent from "./components/Home/header/header";
 
 import AdministratorComponet from "./components/Administrator";
@@ -9,7 +8,7 @@ import HomeComponet from "./components/Home";
 import CommunityComponet from "./components/Community";
 import SupportComponet from "./components/Support";
 import { Link, Routes, Route } from "react-router-dom";
-
+import UserComponent from "./components/User";
 import BugReportContainer from "./components/Support/BugReport/Container";
 
 import Menubar from "./components/Home/menubar/Menubar";
@@ -17,22 +16,22 @@ import Menubar from "./components/Home/menubar/Menubar";
 function App() {
   return (
     <div>
-      <HomeComponet />
-      <UserComponet />
-      <CommunityComponet />
+      <HeaderComponent />
+      <UserComponent />
 
       {/* 나중에 합칠 때 홈 안에 커뮤니티 컴포넌트를 넣어야 한다. */}
-      
-      <Link to={"/"}>메인홈</Link>{" | "}
-      <Link to={"/Community/Free"}>커뮤니티</Link><br />
+
+      <Link to={"/"}>메인홈</Link>
+      {" | "}
+      <Link to={"/Community/Free"}>커뮤니티</Link>
+      <br />
       <Routes>
         <Route path="/Community/*" element={<CommunityComponet />}></Route>
       </Routes>
 
-      <CommunityComponet />
       <Link to={"/Support"}>고객지원</Link>
       <Routes>
-          <Route path="/" element={<HomeComponet />}></Route>
+        <Route path="/" element={<HomeComponet />}></Route>
         <Route path="/news"></Route>
         <Route path="/Support/*" element={<SupportComponet />}></Route>
 
@@ -42,8 +41,6 @@ function App() {
         ></Route>
       </Routes>
       <AdministratorComponet />
-
-
     </div>
   );
 }

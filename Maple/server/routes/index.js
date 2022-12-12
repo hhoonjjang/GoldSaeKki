68635654066c6ad0a3db1b4c779521a3f204c4a5
hhoonjjang <111038171+hhoonjjang@users.noreply.github.com>
2022-12-13 08:47:43 +0900

    1213

--- Maple/server/routes/index.js
+++ Maple/server/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import user from "./user.js";
-
+import report from "./report.js";
 const router = Router();
-
+console.log("api와따");
+router.use("/report", report);
 router.use("/join", user);
 
 export default router;

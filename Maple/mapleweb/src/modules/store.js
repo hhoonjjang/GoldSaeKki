5d56a212cc36d03ae3f3003208b593f55ccf35fa
hhoonjjang <111038171+hhoonjjang@users.noreply.github.com>
2022-12-13 09:20:00 +0900

    1213수정

--- Maple/mapleweb/src/modules/store.js
+++ Maple/mapleweb/src/modules/store.js
@@ -5,10 +5,15 @@ import { initialize as registIni, reducer as registReducer } from "./regist";
 
 import { initialize as userIni, reducer as userReducer } from "./user";
 
-import { initialize as reportIni, reducer as reportReduce, header: headerReducerr } from "./support";
+import { initialize as reportIni, reducer as reportReducer } from "./support";
 const store = createStore(
-  combineReducers({ regist: registReducer, reportReducer ,user: userReducer}),
-  { regist: registIni, report: reportIni , header: headerIni, user: userIni},
+  combineReducers({
+    regist: registReducer,
+    report: reportReducer,
+    user: userReducer,
+    header: headerReducer,
+  }),
+  { regist: registIni, report: reportIni, header: headerIni, user: userIni },
 
   composeWithDevTools()
 );

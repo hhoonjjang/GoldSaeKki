6326b26ba25effea24e533084eb4d90074bb4f56
Koh-U-Seok <kosh1668@naver.com>
2022-12-15 16:27:43 +0900

    1215 16:27

--- Maple/mapleweb/src/components/User/Login/Container.jsx
+++ Maple/mapleweb/src/components/User/Login/Container.jsx
@@ -1,21 +1,23 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import LoginComponent from "./Component";
 import axios from "axios";
 import { action } from "../../../modules/user";
 import crypto from "crypto-js";
 
-let currUser = ""; // 현재 로그인한 아이디를 담을 변수
+let currUser = "";
 const LoginContainer = () => {
   const dispatch = useDispatch();
+  // const currUserName = useSelector((store) => store)
   // let tempCookie = document.cookie.split("=");
   // let cookieJwt = tempCookie[1];
 
   const loginCheck = () => {
     if (document.cookie) {
       axios.post("http://localhost:8080/api/user/logincheck").then((data) => {
+        dispatch(action.check(data.data.userInfo.name));
         console.log("로그인정보를 받았다", data);
-        currUser = data.data.userInfo.name;
-        console.log(currUser);
+        // currUser = data.data.userInfo.name;
+        // console.log(currUser);
       });
     }
   };

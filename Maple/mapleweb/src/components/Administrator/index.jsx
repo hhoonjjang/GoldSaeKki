499e0123a4e22e2be5c44789700f8a960cd817c2
hhoonjjang <111038171+hhoonjjang@users.noreply.github.com>
2022-12-15 21:14:44 +0900

    나는그만할래

--- Maple/mapleweb/src/components/Administrator/index.jsx
+++ Maple/mapleweb/src/components/Administrator/index.jsx
@@ -3,11 +3,13 @@ import { action } from "../../modules/header";
 import styled from "styled-components";
 import BugCSContainer from "./BugCs/Container";
 import AccountContainer from "./Account/Container";
+import AdminLoginContainer from "./AdminLogin/Container";
 const AdministratorComponet = () => {
   const dispatch = useDispatch();
   dispatch(action.header("Administrator"));
   return (
     <AdminBox>
+      <AdminLoginContainer />
       <AccountContainer />
       <BugCSContainer />
     </AdminBox>

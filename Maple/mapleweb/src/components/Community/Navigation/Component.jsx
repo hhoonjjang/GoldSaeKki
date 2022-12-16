2d2b076844af07e21f0b10f3c1495d366382c956
kawonlee <shadow4634@naver.com>
2022-12-16 20:58:08 +0900

    1216 mypage

--- Maple/mapleweb/src/components/Community/Navigation/Component.jsx
+++ Maple/mapleweb/src/components/Community/Navigation/Component.jsx
@@ -1,18 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 // import { Link, Routes, Route } from "react-router-dom";
 import { Link, useLocation } from "react-router-dom";
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
 // 카테고리 저장을 위한 Redux
 import { useDispatch } from "react-redux";
-import { action } from '../../../modules/community';
+import { action } from "../../../modules/community";
 
 // ##. 네비게이션 컴포넌트 사용 방법
 // #1. 사용하고 싶은 파일에서 컴포넌트를 import한다.
 // import NavigationComponent from "./Navigation/Component";
 // #2. 네비게이션을 사용하고 싶은 부분에 컴포넌트를 넣어준다.
 // <NavigationComponent categorys={CATEGORY} />
-// #3. 네비게이션 컴포넌트 사용을 위해 상위 컴포넌트(이 컴포넌트를 사용하는 곳)에서 
+// #3. 네비게이션 컴포넌트 사용을 위해 상위 컴포넌트(이 컴포넌트를 사용하는 곳)에서
 // 이처럼 카테고리와 라우터 주소를 작성하고, categorys라는 이름의 props로 보내주면 된다.
 // const CATEGORY = [
 //     { name: '자유게시판', label: "Free", link: "/Free" },
@@ -29,139 +29,131 @@ import { action } from '../../../modules/community';
 //   ... 등등
 // </Routes>
 
-
 // 만약 호버하면
 // 어떤 놈을 호버했는지 번호를 알아내서..
 // 나머지 앞부분 놈들 위드를 알아내고, 얼만큼 떨어졌는지 알아내서
-// 
+//
 
 const NavigationComponent = ({ categorys }) => {
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-    const dispatch = useDispatch();
-    const location = useLocation();
+  // 주소 이름은 locationName.current를 통해 가져올 수 있다.
+  const locationName = useRef("");
+  // 현재 몇 번째 카테고리인지 nowRouterIdx.current를 통해 가져올 수 있다.
+  const nowRouterIdx = useRef(0);
 
-    // 주소 이름은 locationName.current를 통해 가져올 수 있다.
-    const locationName = useRef("");
-    // 현재 몇 번째 카테고리인지 nowRouterIdx.current를 통해 가져올 수 있다.
-    const nowRouterIdx = useRef(0);
+  // 페이지 리랜더링
+  const [_, render] = useState(true);
 
-    // 페이지 리랜더링
-    const [_, render] = useState(true);
+  // 라우터 주소가 바뀌면 해당 라우터 이름을 반환한다.
+  useEffect(() => {
+    render(!_);
+    const locationArr = location.pathname.split("/");
+    locationName.current = locationArr[locationArr.length - 1];
+  }, [location]);
 
-    // 라우터 주소가 바뀌면 해당 라우터 이름을 반환한다.
-    useEffect(() => {
-        render(!_);
-        const locationArr = location.pathname.split("/");
-        locationName.current = locationArr[locationArr.length - 1];
-    }, [location]);
+  // 현재 라우터 이름이 카테고리의 몇번째인지 알아내 ref에 저장해준다.
+  categorys.forEach((category, index) => {
+    if (category.label === locationName.current) {
+      nowRouterIdx.current = index;
+    }
+  });
 
-    // 현재 라우터 이름이 카테고리의 몇번째인지 알아내 ref에 저장해준다.
-    categorys.forEach((category, index) => {
-        if (category.label === locationName.current) {
-            nowRouterIdx.current = index;
-        }
-    });
+  return (
+    <NavigationWrap className="mnb_wrap">
+      <NavigationBox className="div_inner2">
+        {categorys.map((item, idx) => {
+          return (
+            <LiBox className="liBox" key={`listbox-${item.label}`}>
+              <Link
+                key={`list-${item.label}`}
+                // to : 해당 카테고리 라우터로 이동한다.
+                to={`./${item.label}`}
+                // className : 내가 선택한 카테고리(라우터에서 가져옴)랑 같으면 띄우도록 해야한다.
+                className={`${idx === nowRouterIdx.current ? "active" : ""}`}
+                // onClick={()=>{
+                //     return dispatch(action.category(`${item.label}`));
+                // }}
+              >
+                <CategoryLi key={`category-${item.label}`}>
+                  {item.name}
+                </CategoryLi>
+              </Link>
+            </LiBox>
+          );
+        })}
 
-
-    return (
-        <NavigationWrap className='mnb_wrap'>
-            <NavigationBox className='div_inner2'>
-
-                {categorys.map((item, idx) => {
-                    return (
-                        <LiBox className='liBox' key={`listbox-${item.label}`}>
-                            <Link
-                                key={`list-${item.label}`}
-                                // to : 해당 카테고리 라우터로 이동한다.
-                                to={`./${item.label}`}
-                                // className : 내가 선택한 카테고리(라우터에서 가져옴)랑 같으면 띄우도록 해야한다.
-                                className={`${idx === nowRouterIdx.current ? "active" : ""}`} 
-                                // onClick={()=>{
-                                //     return dispatch(action.category(`${item.label}`));
-                                // }}
-                                >
-                                <CategoryLi key={`category-${item.label}`}>{item.name}</CategoryLi>
-                            </Link>
-
-                        </LiBox>
-                    
-                    )
-                })}
-
-                {/* 계산해서 width, left값 변경하기 */}
-                <CategoryLine className='mnb_line'></CategoryLine>
-
-            </NavigationBox>
-        </NavigationWrap>
-
-    );
-
+        {/* 계산해서 width, left값 변경하기 */}
+        <CategoryLine className="mnb_line"></CategoryLine>
+      </NavigationBox>
+    </NavigationWrap>
+  );
 };
 
 export default NavigationComponent;
 
 const NavigationWrap = styled.div`
-    font-size: 20px;
-    height: 66px;
-    background-color: white;
-    position: sticky;
-    top: 62px;
-    border-top: 1px solid #ebebeb;
-    border-bottom: 1px solid #ebebeb;
-    z-index: 1;
-    
-    width:100%; 
-    float:left; 
+  font-size: 20px;
+  height: 66px;
+  background-color: white;
+  position: sticky;
+  top: 62px;
+  border-top: 1px solid #ebebeb;
+  border-bottom: 1px solid #ebebeb;
+  z-index: 1;
+
+  width: 100%;
+  float: left;
 `;
 
 const NavigationBox = styled.div`
-    margin: 0 auto;
-    width: 1200px;
-    height: inherit;
-    position: relative;
-    overflow: hidden;
+  margin: 0 auto;
+  width: 1200px;
+  height: inherit;
+  position: relative;
+  overflow: hidden;
 `;
 
 const LiBox = styled.div`
+  height: inherit;
+  display: inline-block;
+  li {
+    font-size: 16px;
+    list-style: none;
+    cursor: pointer;
     height: inherit;
-    display: inline-block;
-    li{
-        font-size: 16px;
-        list-style: none;
-        cursor: pointer;
-        height: inherit;
-        line-height: 64px;
-        text-align: left;
-        background-color: white;
-        margin-right: 44px;
+    line-height: 64px;
+    text-align: left;
+    background-color: white;
+    margin-right: 44px;
+  }
+  a {
+    color: #666;
+    text-decoration: none;
+    // a 호버
+    &:hover {
+      color: #242424;
     }
-    a{
-        color: #666;
-        text-decoration: none;
-        // a 호버
-        &:hover{
-            color: #242424;
-        }
-        /* a중에서 active인 class를 가진 li */
-        &.active li{
-            color: #333;
-            font-weight: 600;
-        }
+    /* a중에서 active인 class를 가진 li */
+    &.active li {
+      color: #333;
+      font-weight: 600;
     }
+  }
 `;
 
-const CategoryLi = styled.li`
-`;
+const CategoryLi = styled.li``;
 
 const CategoryLine = styled.span`
-    position:absolute; 
-    bottom:-1px; 
-    height:5px; 
-    background-color:#434343; 
-    /* 현재는 이렇게 설정해놨음 */
-    /* left:32px;  */
-    left: 0px;
-    /* 기본 위드는 20px으로 설정해놨음 */
-    width:20px;
-    /* 현재 카테고리의 위드만큼으로 바꾸기 */
+  position: absolute;
+  bottom: -1px;
+  height: 5px;
+  background-color: #434343;
+  /* 현재는 이렇게 설정해놨음 */
+  /* left:32px;  */
+  left: 0px;
+  /* 기본 위드는 20px으로 설정해놨음 */
+  width: 20px;
+  /* 현재 카테고리의 위드만큼으로 바꾸기 */
 `;
--- Maple/mapleweb/src/components/User/MyPage/UserOut/Component.jsx
+++ Maple/mapleweb/src/components/User/MyPage/UserOut/Component.jsx
@@ -1,7 +1,56 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const UserOutComponent = () => {
-  return <div>회원탈퇴 페이지임니다.</div>;
+  return (
+    <UserOutBox>
+      <p className="alert-msg">
+        회원 탈퇴 후 복구가 불가능하니 신중하게 결정하시기 바랍니다.
+      </p>
+      <UserOutBtnBox>
+        <button>회원 탈퇴</button>
+        <Link to={"/mypage"}>
+          <button>취소</button>
+        </Link>
+      </UserOutBtnBox>
+    </UserOutBox>
+  );
 };
 
 export default UserOutComponent;
+
+const UserOutBox = styled.div`
+  border: 1px solid #5e7bcb;
+  .alert-msg {
+    font-size: 20px;
+    font-weight: 600;
+    color: black;
+  }
+`;
+
+const UserOutBtnBox = styled.div`
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & button:first-child {
+    width: 150px;
+    height: 50px;
+    border: none;
+    border-radius: 5px;
+    background-color: #5e7bcb;
+    color: white;
+    font-size: 18px;
+  }
+
+  & button:last-child {
+    width: 150px;
+    height: 50px;
+    margin-left: 10px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    background-color: white;
+    color: #5e7bcb;
+  }
+`;

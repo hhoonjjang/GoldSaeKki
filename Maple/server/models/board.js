75eb34275cf0096139003196ba1f8176c74b7352
efforthye <efforthye@gmail.com>
2022-12-23 16:35:39 +0900

    리베이스시 사라진 것 추가

--- Maple/server/models/board.js
+++ Maple/server/models/board.js
@@ -50,11 +50,18 @@ export default class Board extends Sequelize.Model {
     }
     static associate(db) {
 
-    // 게시물들을 유저에 연결시켜줌
-    db.Board.belongsTo(db.User, {
-      targetKey: "userName", // id -> userName (관계 맺었을때 가져오는 값)
-      foreignKey: "userName", // 생성 컬럼 이름
-      onDelete: "cascade",
-    });
-  }
+        // 게시글이 댓글들을 가짐 : 댓글에 게시글 아이디를 추가해준다.
+        db.Board.hasMany(db.Comment, {
+            as: "BoardComments",      // 메서드명
+            sourceKey: "id",   // 위에서 id 불러오겠다
+            foreignKey: "boardId",   // 생성 컬럼 이름
+        });
+
+        // 게시물들을 유저에 연결시켜줌
+        db.Board.belongsTo(db.User, {
+            targetKey: "userName", // id -> userName (관계 맺었을때 가져오는 값)
+            foreignKey: "userName", // 생성 컬럼 이름
+            onDelete: "cascade",
+        });
+    }
 }

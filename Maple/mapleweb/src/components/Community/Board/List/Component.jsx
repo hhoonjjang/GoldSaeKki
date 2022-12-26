9ebafd1acc7123c7cbf85361720f556a744df072
efforthye <efforthye@gmail.com>
2022-12-26 09:09:38 +0900

    버튼 드래그 금지

--- Maple/mapleweb/src/components/Community/Board/List/Component.jsx
+++ Maple/mapleweb/src/components/Community/Board/List/Component.jsx
@@ -31,7 +31,7 @@ const ListComponent = () => {
 
   // 주소의 값으로 카테고리 이름을 찾아 기본값으로 저장
   const [category, setCategory] = useState(CATEGORY.find(item => item.label == nowParam));
-  const [_,render] = useState(false);
+  const [_, render] = useState(false);
 
   // 페이징 처리 : 현재 페이지
   const [nowPage, setNowPage] = useState(1);
@@ -58,7 +58,7 @@ const ListComponent = () => {
 
     // 월드 초기화
     const active2 = document.querySelectorAll(".active2");
-    for(let i = 0 ; i<active2.length ; i++){
+    for (let i = 0; i < active2.length; i++) {
       active2[i].classList.remove("active2");
     }
     allWorldRef.current.classList.add("active2");
@@ -71,19 +71,19 @@ const ListComponent = () => {
   let worldBoards = [];
 
   // 현재 월드가 바뀔 때 // 여기부터
-  useEffect(()=>{
+  useEffect(() => {
     // console.log(nowWorld);
     // console.log(boards);
     // boards에서 world이름이 nowWorld이름과 같은 것만 다시 boards에 저장 -> 리덕스에도 재저장
 
-    if(nowWorld=="전체월드"){
+    if (nowWorld == "전체월드") {
       console.log("전체월드임");
       return;
-    }else{
-      console.log("전체월드아님"+nowWorld+"임");
-      boards?.map((item, idx)=>{
+    } else {
+      console.log("전체월드아님" + nowWorld + "임");
+      boards?.map((item, idx) => {
         // item.world == nowWorld
-        if(item.world == nowWorld){
+        if (item.world == nowWorld) {
           // console.log(item);
           // 새로운 배열에 item들만 다시 담아준 뒤 boards를 대체시켜준다.
           worldBoards.push(item);
@@ -167,7 +167,7 @@ const ListComponent = () => {
         {/* 월드 선택 */}
         <WorldBox>
           {WORLDLIST.map((item, idx) => {
-            
+
             return (
               // 현재 선택된 월드 allWorldRef
               <WorldSpan key={`world-${idx}`} className={`${idx == 0 ? "active2" : ""}`} ref={idx === 0 ? allWorldRef : noRef} onClick={(e) => {
@@ -175,7 +175,7 @@ const ListComponent = () => {
                 // 해당 클래스를 가진 놈들
                 const active2 = document.querySelectorAll(".active2");
                 // 다 삭제해버림
-                for(let i = 0 ; i<active2.length ; i++){
+                for (let i = 0; i < active2.length; i++) {
                   active2[i].classList.remove("active2");
                 }
                 // 내가 클릭한 놈한테 active 클래스 추가
@@ -460,6 +460,13 @@ const WorldSpan = styled.span`
   position: relative;
   cursor: pointer;
   padding-left: 8px;
+
+  /* 드래그 금지 */
+  -webkit-touch-callout: none;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-select: none;
   
   /* 이놈 : 자식 전부를 뜻함 */
   *{

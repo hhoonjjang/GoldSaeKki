63fd08ccfae2662f9f91db57b70feaca65d1cb45
Koh-U-Seok <kosh1668@naver.com>
2022-12-28 20:07:00 +0900

    1228 20:06

--- Maple/mapleweb/src/components/Home/mainSearchResult/MainSearchResultComponent.jsx
+++ Maple/mapleweb/src/components/Home/mainSearchResult/MainSearchResultComponent.jsx
@@ -19,14 +19,15 @@ const MainSearchResultComponent = ({
     <MainSearchResult>
       <div className="mainSearchResult_innerBox">
         <div className="mainSearchResult_innerBox_summaryBox">
-          '<span>{searchData}</span>'(으)로 '<span>{searchType}</span>' 검색한 결과입니다.
+          '<span>{searchData}</span>'(으)로 '<span>{searchType}</span>' 검색한
+          결과입니다.
         </div>
         <table className="mainSearchResult_innerBox_resultTable">
           <tbody>
-            <tr>
-              <td>공지사항</td>
-              <td></td>
-            </tr>
+            {/* <tr> */}
+            {/* <td>공지사항</td> */}
+            {/* <td></td> */}
+            {/* </tr> */}
             <tr>
               <td>커뮤니티</td>
               <td>
